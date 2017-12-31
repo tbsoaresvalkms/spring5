de4bf58add9c88d13a30b6f09ad25dbfc689bea9
Thiago b Soares <tbsoaresvalkms@gmail.com>
2017-12-31 01:42:35 -0200

    JoinTable, joinColumns, inverseJoinColumns

--- src/main/java/com/tbsoaresvalkms/spring5webapp/models/Book.java
+++ src/main/java/com/tbsoaresvalkms/spring5webapp/models/Book.java
@@ -18,5 +18,8 @@ public class Book {
     private String isbn;
     private String publisher;
     @ManyToMany
+    @JoinTable(name = "author_book",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 }
